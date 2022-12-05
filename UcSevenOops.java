62ad57f2eafc11110e950500de2110de718bbdc5
shaik0123 <shaik.ismail8563@gmail.com>
2022-12-05 13:28:43 +0530

    day-8 assignment

---
+++ UcSevenOops.java
@@ -0,0 +1,38 @@
+package day8;
+
+public class UcSevenOops {
+    public static final int IS_PART_TIMEn= 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HOURS_IN_MONTH = 10;
+    public static final int MAX_HRS_IN_MONTH = 10;
+
+    public static int computeEmpWage(){
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingdays = 0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingdays < NUM_OF_WORKING_DAYS){
+            totalWorkingdays++;
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            switch (empCheck) {
+                case IS_PART_TIMEn:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Day#:" +totalWorkingdays+"emp hr:" +empHrs);
+
+        }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total Emp Wage:" +totalEmpWage);
+        return totalEmpWage;
+    }
+    public static void main(String[] args){
+        computeEmpWage();
+    }
+}
+
